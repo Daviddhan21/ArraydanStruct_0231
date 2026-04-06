5adfadd07cc364ca715ff1ee3a97ed1418b816bd
Daviddhan21 <rayvanza.davida.ft24@mail.umy.ac.id>
2026-04-06 17:35:34 +0700

    menampilkan dari gambar

--- array.cpp
+++ array.cpp
@@ -1,13 +1,5 @@
 
 
-    cout << endl;
-
-     // Menampilkan dengan menggunakan perulangan for (dari gambar)
-    for (int i = 0; i <= 4; i++)
-    {
-        cout << "Data ke-" << i+1 << " = " << kamu[i] << endl;
-    }
-
 
     return 0;
 }
